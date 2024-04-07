8bc5d400c13d9e210a7d195278f06da9be93a51d
Ryan Fong <rybeats@live.com>
2024-04-07 12:56:08 -0700

    Built docs for main

--- dev.sh
+++ dev.sh
@@ -1,19 +1,20 @@
 # set of commands to run, build, test, and document the project
 if [ "$1" == "run" ]; then
-    echo "Running the project..."
-    gleam run
+	echo "Running the project..."
+	gleam run
 elif [ "$1" == "build" ]; then
-    echo "Building the project..."
-    gleam build
+	echo "Building the project..."
+	gleam build
 elif [ "$1" == "test" ]; then
-    echo "Testing the project..."
-    gleam test
+	echo "Testing the project..."
+	gleam test
 elif [ "$1" == "docs" ]; then
-    echo "Documenting the project..."
-    rm -rf docs
-    gleam docs build
-    mv build/dev/docs/html_dsl docs
+	echo "Documenting the project..."
+	rm -rf docs
+	gleam docs build
+	mv build/dev/docs/html_dsl docs
 else
-    echo "Usage: $0 {run|build|test|doc}"
-    exit 1
+	echo "Usage: $0 {run|build|test|docs}"
+	exit 1
 fi
+
